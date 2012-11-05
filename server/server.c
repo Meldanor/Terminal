f79d8ad99942f39b4ca3ce8366d94acc19936475
Kilian Gärtner <Bhaals@gmx.de>
2012-11-05 16:54:01 +0100

    Check if port is a number

--- server/server.c
+++ server/server.c
@@ -58,13 +58,12 @@ int main(int argc, char **args) {
         cur = args[i];
         // READ PORT
         if (strcmp(cur, "-p") == 0) {
-            // CONVERT THE STRING TO AN INTEGER
-            // TODO: Use a method that checqks if it is an valid number
-            port = strtol(args[++i], (char **) NULL, 10);
-            if (port == LONG_MIN || port == LONG_MAX) {
-                printf("Port %s is an invalid number!\n", args[i]);
+            // Check if the user has written a number
+            if (!isNumber(args[++i])) {
+                printf("Port '%s' is an invalid number!\n", args[i]);
                 return EXIT_FAILURE;
-            }
+            }            
+            port = strtol(args[i], (char **) NULL, 10);
             // DISALLOW KERNEL PORTS
             if (port <= 1024) {
                 printf("Port %ld must be higher than 1024!\n", port);
